9a760140398f728e11be76c093b363e4235c108a
Nathan <nhwilcox@gmail.com>
2016-05-27 11:12:15 -0700

    remove queue

--- main.js
+++ main.js
@@ -5,8 +5,6 @@ var margin = {
     left: 70
 }
 
-// WE NEED TOP CROPS BY ACRES FOR EACH COUNTY
-
 var width = 1000 - margin.left - margin.right;
 var height = 500 - margin.top - margin.bottom;
 
@@ -16,13 +14,13 @@ var svg = d3.select("body").append("svg")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	var countyName = "Hood River";
+	var countyName = "Washington";
+	// var countyName = document.getElementById('countySelect');
 
-queue()
-    .defer(d3.json, "http://api.cropcompass.org/data/nass_commodity_area?region=" + countyName, function(d) {
+d3.json("http://api.cropcompass.org/data/nass_commodity_area?region=" + countyName, function(d) {
 		d.data.sort(function(a,b) {
 			return b.acres - a.acres;
-		})
+		});
 
 		console.log(d.data[0].commodity + " " +
 					d.data[1].commodity + " " +
@@ -30,26 +28,7 @@ queue()
 					d.data[3].commodity + " " +
 					d.data[4].commodity);
 
-        // console.log(d.data[0].region);
-
-		// for(var i=0; i<d.data.length; i++) {
-		// 	console.log(d.data[i]);
-		// }
-
-
-
+		// d3.select("#countySelect").on("input", function() {
+		// 	update(+this.value);
+		// });
     })
-    // .defer(d3.json, "http://api.cropcompass.org/data/nass_animals_inventory?commodity="+commodity, function(d) {
-    // 	for(var i=0; i<d.length; i++) {
-    // 		var
-    // 	}
-    // 	console.log(d.data[0].animals)
-    //     map.set(d.id, +d.rate);
-    // })
-    .await(ready);
-
-function ready(error, crops) {
-    if (error) throw error;
-
-
-}
