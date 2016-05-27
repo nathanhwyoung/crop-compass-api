dbcdf1a5a729699d868eaf20102a7e2a10b1ee9d
Nathan <nhwilcox@gmail.com>
2016-05-27 15:51:14 -0700

    add charts

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 var width = 1000;
 var height = 500;
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g");
+//
+// var svg = d3.select("body").append("svg")
+//     .attr("width", width)
+//     .attr("height", height)
+//     .append("g");
 // .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var countyName = "Baker";
+var countyName = "Washington";
 // var countyName = document.getElementById('countySelect');
-
-
 
 d3.json("http://api.cropcompass.org/data/nass_commodity_area?region=" + countyName, function(d) {
     d.data.sort(function(a, b) {
@@ -22,13 +20,18 @@ d3.json("http://api.cropcompass.org/data/nass_commodity_area?region=" + countyNa
 
     // THESE VALUES WILL NEED TO BE PLAYED WITH
     var xScale = d3.scale.linear()
-        .domain([0, d3.max(dataset)])
-        .range([0, 420]);
+        .domain([0, d3.max(dataset, function(d) { return d3.max(d.acres); })])
+        .range([0, width]);
 
-    var yScale = d3.scale.linear()
-        .domain([0, dataset.length])
-        .range([0, 100]);
+		console.log(d3.max(dataset));
+		console.log(dataset);
 
+    // var yScale = d3.scale.linear()
+    //     .domain([0, dataset.length])
+    //     .range([0, 480]);
+
+		var colorScale = d3.scale.ordinal()
+		.range(["#5EAA00", "#87B725", "#A1C02A", "#BCCA30", "#E1D837"]);
     // console.log(d.data[0].commodity + " " +
     // 			d.data[1].commodity + " " +
     // 			d.data[2].commodity + " " +
@@ -40,12 +43,18 @@ d3.json("http://api.cropcompass.org/data/nass_commodity_area?region=" + countyNa
         .data(dataset)
         .enter()
 		.append("div")
-        .style("width", function(d) {
-            return (d.acres/100) + "px";
+		.style("border-radius", "3px")
+		.style("height", "20px")
+        // .style("width", function(d) {
+        //     return xScale(d.acres) + "px";
+        // })
+		.style("width", function(d) {
+            return xScale(d.acres) + "px";
         })
-        // .text(function(d) {
-        //     return d;
-        // });
+		.style("background-color", function(d) { return colorScale(d.acres); })
+        .text(function(d) {
+            return d.commodity;
+        });
 
     // var bars = svg.selectAll("div")
     //     .data(dataset)
@@ -58,8 +67,6 @@ d3.json("http://api.cropcompass.org/data/nass_commodity_area?region=" + countyNa
     // 		'y': 50
     // 	})
     // 	.attr("fill", "black");
-
-    console.log(dataset);
 
     // d3.select("#countySelect").on("input", function() {
     // 	update(+this.value);
